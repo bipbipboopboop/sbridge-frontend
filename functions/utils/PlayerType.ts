fb6ceea6a28e1adfdb6b1020696a4bec2884fdb3
Ethan <ethanchen2009@hotmail.com>
2022-12-27 11:39:52 +0800

    Remove firebase functions for testing

--- functions/utils/PlayerType.ts
+++
@@ -1,5 +0,0 @@
-export type Player = {
-  uid: string; // player's uid
-  playerName: string; // player's name
-  roomID: string | null; // default : null
-};
--- functions/utils/RoomType.ts
+++
@@ -1,7 +0,0 @@
-export type Room = {
-  currNumPlayers: number;
-  gameStatus: string;
-  playersUID: [string];
-  roomOwnerName: string;
-  roomOwnerUID: string;
-};
