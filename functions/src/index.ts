96af02516d77b971a347286eeed9ce4b703f986e
Ethan <ethanchen2009@hotmail.com>
2022-12-28 02:44:06 +0800

    Create deletePlayer for testing

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,7 +3,13 @@ import * as functions from "firebase-functions";
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", { structuredData: true });
+//   response.send("Hello from Firebase!");
+// });
+
+export const deletePlayer = functions.https.onCall((data, context) => {
+  return null;
 });
+
+// https://stackoverflow.com/questions/71363950/cannot-deploy-firebase-cloud-functions-because-functions-lib-index-js-does-not
