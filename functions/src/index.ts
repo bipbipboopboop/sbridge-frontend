ea0443512d1b165c65973d63b1e6d8f8f46c38ab
Ethan <ethanchen2009@hotmail.com>
2022-12-28 15:08:02 +0800

    Add brief docs for addPlayer cloud function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -29,19 +29,29 @@ export const deletePlayer = functions.https.onCall(async (data, context) => {
   return result;
 });
 
-export const addPlayer = functions.https.onCall(async (playerName, context) => {
-  if (!context.auth) {
-    const result = { status: "error", code: 401, message: "Not signed in" };
+/**
+ * Adds a player instance in Firestore. Used immediately after User authentication.
+ * Creates a player instance with the following fields :
+ * @field playerName{string}
+ * @field uid{string}
+ * @field roomID{string}
+ * @param playerName{string} Name of the player
+ */
+export const addPlayer = functions.https.onCall(
+  async (playerName: string, context) => {
+    if (!context.auth) {
+      const result = { status: "error", code: 401, message: "Not signed in" };
+      return result;
+    }
+    const playerUID = context.auth.uid;
+    const playerRef = admin.firestore().doc(`players/${playerUID}`);
+    console.log({ playerRef: JSON.stringify(playerRef) });
+    await playerRef.set({ playerName, uid: playerUID, roomID: null });
+    const result = {
+      status: "success",
+      code: 201,
+      message: "Successfully added player",
+    };
     return result;
   }
-  const playerUID = context.auth.uid;
-  const playerRef = admin.firestore().doc(`players/${playerUID}`);
-  console.log({ playerRef: JSON.stringify(playerRef) });
-  await playerRef.set({ playerName, uid: playerUID, roomID: null });
-  const result = {
-    status: "success",
-    code: 201,
-    message: "Successfully added player",
-  };
-  return result;
-});
+);
